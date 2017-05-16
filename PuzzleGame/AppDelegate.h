176464098680a245f8d55f0585f0bbd5ce1dd448
Karen Ghandilyan <ghkaren@gmail.com>
2017-05-17 02:16:26 +0400

    Initial Commit

---
+++ PuzzleGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PuzzleGame
+//
+//  Created by Karen Ghandilyan on 5/17/17.
+//  Copyright © 2017 Wagawin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PuzzleGame/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  PuzzleGame
+//
+//  Created by Karen Ghandilyan on 5/17/17.
+//  Copyright © 2017 Wagawin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
